87647788446a5cba61472498174d45b30a6a86b7
ku2ma2 <motorohi.tsumaniku@gmail.com>
2017-12-30 16:20:05 +0900

    Add ex8.6 in eki8.ml

--- eki8.ml
+++ eki8.ml
@@ -11,3 +11,16 @@ type ekimei_t = {
 }
 
 let ekimei_test1 = {kanji="品川"; kana="しながわ"; romaji="shinagawa"; shozoku="京浜東北線"}
+let ekimei_test2 = {kanji="茗荷谷"; kana="みょうがだに"; romaji="myogadani"; shozoku="丸ノ内線"}
+
+(* 問題 8.6: ekimei_t 型のデータを受け取ってきたら、「路線名、駅名（かな）」
+の形式の文字列を返す関数 hyoji をデザインレシピにしたがって作れ。 *)
+(* 目的: ekimei_t 型のデータを受け取ってきたら、「路線名、駅名（かな）」の形式
+の文字列を返す関数 *)
+(* hyoji : ekimei_t -> string *)
+let hyoji ekimei_t = match ekimei_t with
+    {kanji=k; kana=kn; romaji=r; shozoku=s} -> s ^ "、" ^ k ^ "（" ^ kn ^ "）"
+
+(* テスト *)
+let hyoji_t1 = hyoji ekimei_test1 = "京浜東北線、品川（しながわ）"
+let hyoji_t2 = hyoji ekimei_test2 = "丸ノ内線、茗荷谷（みょうがだに）"
