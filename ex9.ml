f87f1a320dd49dd7064dcbd20f1e549e8d7521b9
ku2ma2 <motorohi.tsumaniku@gmail.com>
2018-01-01 07:55:34 +0900

    Add ex9.5 in practice9

--- ex9.ml
+++ ex9.ml
@@ -17,11 +17,26 @@ person_1 :: person_2 :: person_3 :: [];;
 (* 目的: 整数のリストを受け取ったら、そのリストの長さを返す関数 *)
 (* length : int list -> int *)
 let rec length list = match list with
-      [] -> 0
-    | first :: rest -> 1 + length rest
+    [] -> 0
+  | first :: rest -> 1 + length rest
 
 (* テスト *)
 let length_test1 = length [] = 0
 let length_test3 = length [6] = 1
 let length_test3 = length [2; 1; 6; 4; 7] = 5
+
+(* 問題 9.5 整数のリストを受け取ったら、その中の偶数の要素のみを含むリストを返す関数
+even をデザインレシピにしたがって作れ。
+例えば even [2; 1; 6; 4; 7] は [2; 6; 4] を返す *)
+(* even : int list -> int list *)
+let rec even list = match list with
+    [] -> []
+  | first :: rest -> 
+        if first mod 2 = 0 then first :: even rest
+        else even rest
+
+(* テスト *)
+let even_test1 = even [] = []
+let even_test2 = even [2] = [2]
+let even_test3 = even [2; 1; 6; 4; 7] = [2; 6; 4]
 
