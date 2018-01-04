be653b4ae7b5e0121b3824893287939f39c25399
ku2ma2 <motorohi.tsumaniku@gmail.com>
2018-01-05 07:44:46 +0900

    Add comment for ex10.11

--- eki10.ml
+++ eki10.ml
@@ -16,8 +16,19 @@ let rec romaji_to_kanji station lst = match lst with
     if r = station then n
     else romaji_to_kanji station rest
 
-
 (* テスト *)
 let romaji_to_kanji_t1 = romaji_to_kanji "myogadani" global_ekimei_list = "茗荷谷"
 let romaji_to_kanji_t2 = romaji_to_kanji "nishinippori" global_ekimei_list = "西日暮里"
 let romaji_to_kanji_t3 = romaji_to_kanji "myogadanidani" global_ekimei_list = ""
+
+
+(* 問題 10.11: 漢字の駅名をふたつ（いずれも文字列）と駅間リスト（ekikan_t list型）
+を受け取ったら、駅間リストの中からその2駅間の距離を返すを返す関数 get_ekikan_kyori
+をデザインレシピにしたがって作れ。問題 9.10で作成した global_ekikan_listを使うと、例えば
+```
+get_ekikan_kyori "茗荷谷" "新大塚" global_ekikkan_list
+```
+は 1.2 を返す。2駅が直接繋がっている場合のみその距離を返し、直接繋がっていない場合は無限大
+infinity を返すようにせよ。 *)
+
+
