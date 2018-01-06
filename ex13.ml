a22765ec8ee8c61da82e4b750215d48c55d6fa92
ku2ma2 <motorohi.tsumaniku@gmail.com>
2018-01-06 21:18:59 +0900

    Add file ex13.ml

---
+++ ex13.ml
@@ -0,0 +1,9 @@
+#use "ex8.ml";;
+
+(* 問題 13.1: 問題8.3 で定義した person_t 型のリストを受け取ったら、
+その中から指定された血液型の人の数を返す関数 count_ketsueki を
+デザインレシピにしたがって作れ *)
+(* count_ketsueki : person_t list -> int *)
+
+
+(* テスト *)
