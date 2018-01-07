114594eb43d5dc930606e6fd28ca96a2de7b42e4
ku2ma2 <motorohi.tsumaniku@gmail.com>
2018-01-07 09:42:25 +0900

    Add Ex13.2 in practice 13

--- ex13.ml
+++ ex13.ml
@@ -19,3 +19,22 @@ let count_ketsueki_t2 = count_ketsueki person_list_t2 "A型" = 2
 let count_ketsueki_t3 = count_ketsueki person_list_t2 "B型" = 0
 let count_ketsueki_t4 = count_ketsueki person_list_t2 "AB型" = 1
 
+
+(* 問題 13.2: 問題8.3で定義した person_t 型のリストを受け取ったら、
+その中に出てくる人の名前のリストを返す関数 person_namae を
+デザインレシピに従って作れ。 *)
+(* take_namae : person_t -> string *)
+let take_namae person = match person with
+    {name=namae; height=h; weight=w; birthday=b; blood=bl} -> namae
+
+(* テスト *)
+let take_namae_t1 = take_namae person_1 = "太郎"
+let take_namae_t2 = take_namae person_2 = "Jiro"
+let take_namae_t3 = take_namae person_3 = "Hanako"
+
+(* person_namae : person_t list -> string list *)
+let person_namae lst = List.map take_namae lst
+
+(* テスト *)
+let person_name_t1 = person_namae person_list_t1 = []
+let person_name_t2 = person_namae person_list_t2 = ["太郎"; "Jiro"; "Hanako"; "Junko"]
