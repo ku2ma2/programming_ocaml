9c1f8920113963eda42f0478d66ddff342f0aebe
ku2ma2 <motorohi.tsumaniku@gmail.com>
2018-01-09 21:44:44 +0900

    Modified count_ketsueki for ex14.6

--- ex14.ml
+++ ex14.ml
@@ -1,3 +1,5 @@
+#use "ex8.ml";;
+
 (* 問題 14.1: 問題 9.5で作成した関数 even を filter 
 を用いて定義せよ。 *)
 
@@ -22,14 +24,32 @@ let rec fold_right f lst init = match lst with
     [] -> init
   | first :: rest -> f first (fold_right f rest init)
 
-(* 目的: 文字列を後方の結果に連結する関数 *)
-let append_str first rest_result = first ^ rest_result
-
 (* concat : string list -> string *)
-let concat lst = fold_right append_str lst ""
+let concat lst = 
+    let append_str first rest_result = first ^ rest_result in 
+        fold_right append_str lst ""
 
 (* テスト *)   
 let concat_test1 = concat [] = ""
 let concat_test2 = concat ["春"] = "春"
 let concat_test3 = concat ["春"; "夏"; "秋"; "冬"] = "春夏秋冬"
+
+
+(* 問題 14.6: 13.1 節に示した関数 count を filter と length 
+を用いて定義せよ。 *)
+(* count_ketsueki : person_t list -> string -> int *)
+let count_ketsueki lst blood = 
+    let is_blood person = match person with
+        {name=n; height=h; weight=w; birthday=b; blood=bl} -> 
+            bl = blood in 
+                List.length ( List.filter is_blood lst )
+
+(* テスト *)
+let person_list_t1 = []
+let person_list_t2 = [person_1; person_2; person_3; person_4]
+
+let count_ketsueki_t1 = count_ketsueki person_list_t1 "A型" = 0
+let count_ketsueki_t2 = count_ketsueki person_list_t2 "A型" = 2
+let count_ketsueki_t3 = count_ketsueki person_list_t2 "B型" = 0
+let count_ketsueki_t4 = count_ketsueki person_list_t2 "AB型" = 1
 
