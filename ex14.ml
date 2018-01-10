bffc1263633e6389b7cae973d19b70b2eb494322
ku2ma2 <motorohi.tsumaniku@gmail.com>
2018-01-11 07:21:08 +0900

    Add ex14.14

--- ex14.ml
+++ ex14.ml
@@ -16,16 +16,16 @@ let even_test3 = even [2; 1; 6; 4; 7] = [2; 6; 4]
 
 
 (* 問題 14.3: 問題 9.6 で作成した関数 concat を fold_right を使って書き直せ *)
+(* 問題 14.14: 問題 9.6 または 問題 14.3 で作成した関数 concat を 1行で定義せよ *)
 
 (* 目的: init から始めて lst の要素を右から順に f を施しこむ *)
 (* fold_right : ('a -> 'b -> 'b) -> 'a list -> 'b -> 'b *)
-let rec fold_right f lst init = match lst with
+(* let rec fold_right f lst init = match lst with
     [] -> init
-  | first :: rest -> f first (fold_right f rest init)
+  | first :: rest -> f first (fold_right f rest init) *)
 
 (* concat : string list -> string *)
-let concat lst = 
-    fold_right (fun f rest_result -> f ^ rest_result ) lst ""
+let concat lst = List.fold_right (^) lst ""
 
 (* テスト *)   
 let concat_test1 = concat [] = ""
