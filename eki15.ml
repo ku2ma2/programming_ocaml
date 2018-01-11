22155d4af754ecce3b8cf33c38b1c640adca36f3
ku2ma2 <motorohi.tsumaniku@gmail.com>
2018-01-11 21:55:28 +0900

    Add file eki15.ml

---
+++ eki15.ml
@@ -0,0 +1,10 @@
+#use "eki8.ml";;
+#use "ekimei.ml";;
+#use "eki10.ml";;
+#use "eki12.ml";;
+#use "eki13.ml";;
+
+(* 問題 15.4: eki_t list 型のリストを受け取ったら、
+「最短距離最小の駅」と「最短距離最小の駅以外からなるリスト」の組
+を返す関数 saitan_wo_bunki をデザインレシピにしたがって作れ *)
+
