5a8fabe47f75a87c6746a4dad668bf36b5f829a7
ku2ma2 <motorohi.tsumaniku@gmail.com>
2018-01-12 08:04:51 +0900

    Add ex15.4

--- eki15.ml
+++ eki15.ml
@@ -7,8 +7,24 @@
 (* 問題 15.4: eki_t list 型のリストを受け取ったら、
 「最短距離最小の駅」と「最短距離最小の駅以外からなるリスト」の組
 を返す関数 saitan_wo_bunki をデザインレシピにしたがって作れ *)
+
+
 (* saitan_wo_bunri : eki_t list -> eki_t * eki_t list *)
-let saitan_wo_bunri lst = ({namae="池袋"; saitan_kyori = infinity; temae_list = []} , [])
+let saitan_wo_bunri lst = match lst with
+    List.fold_right 
+        (
+            fun first (p, v) ->
+                match (first, p) with 
+                    ( {namae=fn; saitan_kyori=fs; temae_list=ft},
+                      {namae=pn; saitan_kyori=ps; temae_list=pt} ) ->
+                        if fn = "" then (first, v) (* 初期値は無視 *)
+                        if fs < ps then (first, p :: v)
+                        else (p, first :: v)
+        )
+        lst (* 適用するリスト *)
+        ({namae=""; saitan_kyori = infinity; temae_list = []}, []) (* 初期値 *)
+
+
 
 (* 駅の例 *) 
 let eki1 = {namae="池袋"; saitan_kyori = infinity; temae_list = []} 
@@ -18,7 +34,8 @@ let eki4 = {namae="後楽園"; saitan_kyori = infinity; temae_list = []}
  
 (* 駅リストの例 *) 
 let lst = [eki1; eki2; eki3; eki4] 
- 
+
 (* テスト *) 
-let saitan_wo_bunki_t1 = saitan_wo_bunri lst = (eki3, [eki1; eki2; eki4]) 
+(* let saitan_wo_bunki_t1 = saitan_wo_bunri lst = (eki3, [eki1; eki2; eki4])  *)
+let saitan_wo_bunki_t1 = saitan_wo_bunri lst
 
