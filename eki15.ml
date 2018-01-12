8bd5b3273f6fe7641d9d4167bf737f465db0da78
ku2ma2 <motorohi.tsumaniku@gmail.com>
2018-01-12 21:41:26 +0900

    Improve comment in Ex15.4

--- eki15.ml
+++ eki15.ml
@@ -17,9 +17,12 @@ let saitan_wo_bunri lst =
                 match (first, p) with 
                     ( {namae=fn; saitan_kyori=fs; temae_list=ft},
                       {namae=pn; saitan_kyori=ps; temae_list=pt} ) ->
-                        if fn = "" then (first, v) (* 初期値は無視 *)
-                        else if fs < ps then (first, p :: v)
-                        else (p, first :: v)
+                      (* 初期値だったら最小の駅を差替えてリストにはなにもしない *)
+                      if pn = "" then (first, v) 
+                      (* 最短距離が小さい駅だった場合は、最小の駅の差替えとリストへの追加 *)
+                      else if fs < ps then (first, p :: v)
+                      (* それ以外はvリストへの追加のみにする p はそのまま p *)
+                      else (p, first :: v) 
         )
         lst (* 適用するリスト *)
         ({namae=""; saitan_kyori = infinity; temae_list = []}, []) (* 初期値 *)
@@ -36,6 +39,6 @@ let eki4 = {namae="後楽園"; saitan_kyori = infinity; temae_list = []}
 let lst = [eki1; eki2; eki3; eki4] 
 
 (* テスト *) 
-(* let saitan_wo_bunki_t1 = saitan_wo_bunri lst = (eki3, [eki1; eki2; eki4])  *)
-let saitan_wo_bunki_t1 = saitan_wo_bunri lst
+let saitan_wo_bunki_t1 = saitan_wo_bunri lst = (eki3, [eki1; eki2; eki4]) 
+(* let saitan_wo_bunki_t1 = saitan_wo_bunri lst *)
 
