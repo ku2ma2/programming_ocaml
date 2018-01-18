bc85b7a242c40bd7e5e191c50bfe90654170866e
ku2ma2 <motorohi.tsumaniku@gmail.com>
2018-01-19 07:59:36 +0900

    Add eki17.10.ml

---
+++ eki17.10.ml
@@ -0,0 +1,11 @@
+(* 問題 17.10: 木の節に
+
+- 漢字の駅名
+- 「『その駅につながっている駅名（漢字）』と『その駅までの距離』の組」
+
+のリストを持つ木の型 ekikan_tree_t を宣言せよ。
+その際、構成子としては Empty と Node とせよ。 *)
+type ekikan_tree_t = 
+    Empty
+  | Node of ekikan_tree_t * string * (string * float) list * ekikan_tree_t
+
