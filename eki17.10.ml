9e787f8a9e827cad0f4dce121914033f359cda36
ku2ma2 <motorohi.tsumaniku@gmail.com>
2018-01-19 08:03:23 +0900

    Add test for eki17.10

--- eki17.10.ml
+++ eki17.10.ml
@@ -9,3 +9,5 @@ type ekikan_tree_t =
     Empty
   | Node of ekikan_tree_t * string * (string * float) list * ekikan_tree_t
 
+
+  let ekikan_tree_t_test = Node (Empty, "茗荷谷", [("新大塚", 1.2); ("後楽園", 1.8)], Empty)
