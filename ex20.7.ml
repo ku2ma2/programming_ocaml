bdbc20444206afab4e817b1f129c46e2f7bb13eb
ku2ma2 <motorohi.tsumaniku@gmail.com>
2018-01-27 09:00:54 +0900

    feat: finish ex20.7

--- ex20.7.ml
+++ ex20.7.ml
@@ -13,4 +13,41 @@ val mem : 'a t -> 'a t -> bool   (* 要素が集合に入っているか *)
 
 end = struct
 
+type 'a t = 'a list
+
+(* 空集合 *)
+(* empty : 'a t *)
+let empty = []
+
+(* 要素ひとつからなる集合 *)
+(* singleton : 'a -> 'a t *)
+let singleton element = [element]
+
+(* 和集合 *)
+(* union : 'a t -> 'a t -> 'a t *)
+let union g1 g2 = g1 @ g2
+
+(* 共通部分 *)
+(* inter : 'a t -> 'a t -> 'a t *)
+let inter g1 g2 = 
+  List.fold_left
+    (fun lst element ->
+      if List.mem element g2 then element :: lst else lst
+    )
+    empty g1
+
+(* 差集合 *)
+(* diff : 'a t -> 'a t -> 'a t *)
+let diff g1 g2 = 
+  List.fold_left
+    (fun lst element ->
+      if List.mem element g2 then lst else element :: lst
+    )
+    empty g1
+
+(* 要素が集合に入っているか *)
+(* mem : 'a t -> 'a t -> bool *)
+let mem element set = List.mem element set
+
+
 end
